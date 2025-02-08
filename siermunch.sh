b59ddadf509631f03d8070b39a28a054c7658873
William Towle <william_towle@yahoo.co.uk>
2025-02-08 14:03:46 +0000

    Add display algorithm
    Port original script (2010-10-26)
    - adapt to tput-based display routines
    - introduce delay routine without use of 'usleep'
    - add trap to reset terminal on receiving signal

--- siermunch.sh
+++ siermunch.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-# (c) and GPLv2 William Towle <william_towle@yahoo.co.uk>
+# (c) and GPLv2 2010-2025 William Towle <william_towle@yahoo.co.uk>
+# Based on a discussion with Stuart Brady <sdb@zubnet.me.uk>
+
+# Draws a sierpinski triangle in a similar manner to munching squares
+# A power of two size argument is required for the effect to work
+
+
+SIZE=$1
+if [ -z "${SIZE}" ] ; then
+	printf '%s: %s\n' $0 'Expected SIZE parameter' 1>&2
+	exit 1
+fi
+shift
+
+DELAY=$(printf '%.3f' $(( 5000 / ($SIZE * $SIZE) )) )e-3
 
 
 plots()
@@ -27,9 +41,28 @@ show1()
 	plots $X $Y ':'
 }
 
+pause()
+{
+	sleep ${DELAY}
+}
+
+display()
+{
+	PLIM=$(( ${SIZE} - 1 ))
+	for P in `seq 0 ${PLIM}` ; do
+		for X in `seq 0 ${PLIM}` ; do
+			Y=$(( $X ^ $P ))
+			[ $(( ( $X ) & ( $PLIM - $P ) )) -eq 0 ] && show0 $Y $X || show1 $Y $X
+
+			pause
+		done
+	done
+
+	tput cup ${SIZE} 0
+}
+
+trap 'tput cup ${SIZE} 0 ; tput sgr0 ; exit 1' 1 2 3 6
+
 tput init
 
-for X in `seq 0 7` ; do
-	[ $(( $X & 1 )) -eq 0 ] && show0 $X $X || show1 $X $X
-	echo
-done
+display ${SIZE}
